ce46124edbd6745a0b867276d969f33297f18c92
Ale <ma.salazarsanchez@gmail.com>
2020-05-09 01:55:10 -0400

    Deleting tweets in DOM and adding new tweets in Local Storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,9 @@ eventListeners();
 function eventListeners(){
   // Cuando se envía el formulario
   document.querySelector('#formulario').addEventListener('submit', agregarTweet);
+
+  // Borrar tweets
+  listaTweets.addEventListener('click', borrarTweet);
 }
 
 
@@ -33,4 +36,40 @@ function agregarTweet(e){
   li.appendChild(botonBorrar)
   // Añade el tweet a la lista
   listaTweets.appendChild(li);
+
+  // Añadir a Local Storage
+  agregarTweetLocalStorage(tweet);
+}
+
+// Elimina el tweet del DOM
+function borrarTweet(e){
+  e.preventDefault();
+  if(e.target.className === 'borrar-tweet'){
+    console.log(e.target.parentElement.remove());
+    alert('Tweet eliminado');
+  }
+}
+
+// Agrega tweet a Local Storage
+function agregarTweetLocalStorage(tweet){
+  let tweets;
+  tweets = obtenerTweetsLocalStorage();
+  // Añadir el nuevo tweet
+  tweets.push(tweet);
+  // Convertir de string a array para Local Storage (si no lo hacemos como array, se reescribe encima uno del otro)) * Local Storage solo graba strings, no numeros ni otro tipo de elementos.
+  localStorage.setItem('tweets', JSON.stringify(tweets));
+  //Agregar a Local Storage
+  localStorage.setItem('tweets', tweet);
+}
+
+// 
+function obtenerTweetsLocalStorage(){
+  let tweets;
+  //Revisamos los valores de Local Storage
+  if(localStorage.getItem('tweets') === null){
+    tweets = [];
+  }else{
+    tweets = JSON.parse(localStorage.getItem('tweets'));
+  }
+  return tweets;
 }
